27bd16acbb06fe9c986ff81b47494e7994d1c625
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-10-13 01:36:22 +0300

    Created blogs#show & route path

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,20 @@
 class BlogsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
     def index
         blogs = Blog.all
         render json: blogs
+    end
+
+    def show
+        blogs = find_blog
+        render json: blogs
+    end
+
+    private
+    def find_blog
+        Blog.find(params[:id])
+    end
+    def render_not_found
+        render json: {"Error": "Blog not found"}, status: :not_found
     end
 end
