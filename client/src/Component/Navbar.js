a6b3968b88fb434b0bfdbd4f085fcc7b60a583f1
Marymuriga <marymuriga@gmail.com>
2022-10-13 00:35:45 +0300

    adding components

---
+++ client/src/Component/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+function Navbar() {
+  return (
+    <div>
+      <h2>Devblog</h2>
+      <div className='nav a'>
+      Home
+      Blog
+      Login
+      Sign-up
+      </div>
+      
+      </div>
+  )
+}
+
+export default Navbar;
