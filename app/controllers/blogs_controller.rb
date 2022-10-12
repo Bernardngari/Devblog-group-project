d7ab16d1985c9d49d029a95147c94c983aa39170
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-10-13 01:48:03 +0300

    Created blogs#create & route path

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_data
     def index
         blogs = Blog.all
         render json: blogs
@@ -10,11 +11,25 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
         render json: blogs
     end
 
+    def create
+        blogs = Blog.create(blogs_params)
+        show, status: :created
+    end
+
     private
     def find_blog
         Blog.find(params[:id])
     end
+
     def render_not_found
         render json: {"Error": "Blog not found"}, status: :not_found
+    end
+
+    def blogs_params
+        params.permit(:title, :content, :blogger_id)
+    end
+
+    def invalid_data(exception)
+        render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
     end
 end
