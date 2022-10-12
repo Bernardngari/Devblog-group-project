6cd817e06b3ec42c2227f1104df3b30f314fb441
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-10-13 00:05:05 +0300

    Created associations and added validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,11 @@
 class Blog < ApplicationRecord
+    belongs_to :blogger
+    has_many :comments, dependent: :destroy
+
+    validates :content, {
+        presence: true,
+        length: {
+            minimum: 100
+        }
+    }
 end
