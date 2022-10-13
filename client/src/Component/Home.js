3dae0d3cd2315f3e21b85d2c83e5a879e01035c9
Marymuriga <marymuriga@gmail.com>
2022-10-13 22:47:04 +0300

    fetching data

--- client/src/Component/Home.js
+++ client/src/Component/Home.js
@@ -1,87 +1,24 @@
-import React from "react";
-// import {Link} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+
 
 function Home() {
+  const [blogs, setBlogs]= useState([]);
+  useEffect (() => {
+    fetch("http://127.0.0.1:3000/blogs")
+    .then((response) => response.json())
+    .then((blogs) => setBlogs(blogs));
+  }, []);
   return (
     <>
       <h1>Welcome to Devblog</h1>
       <div className="container-1">
-        <p>
-          Lorem ipsum is the filler text that typically demonstrates the font
-          and style of a text in a document or visual demonstration. It serves
-          as a place holder indicating where the text will be in the final
-          iteration. Originally from Latin, Lorem ipsum has no intelligible
-          meaning. It is simply a display of letters to be viewed as a sample
-          with given graphical elements in a file. "Lipsum" (a portmanteau of
-          lorem and ipsum) generators are commonly used to form generic text in
-          a file. The words are adequately like normal text to demonstrate a
-          font, without distracting the reader with its content.
-        </p>
-      </div>
-      <div className="container-2">
-        <p>
-          Lorem ipsum is the filler text that typically demonstrates the font
-          and style of a text in a document or visual demonstration. It serves
-          as a place holder indicating where the text will be in the final
-          iteration. Originally from Latin, Lorem ipsum has no intelligible
-          meaning. It is simply a display of letters to be viewed as a sample
-          with given graphical elements in a file. "Lipsum" (a portmanteau of
-          lorem and ipsum) generators are commonly used to form generic text in
-          a file. The words are adequately like normal text to demonstrate a
-          font, without distracting the reader with its content.
-        </p>
-      </div>
-      <div className="container-3">
-        <p>
-          Lorem ipsum is the filler text that typically demonstrates the font
-          and style of a text in a document or visual demonstration. It serves
-          as a place holder indicating where the text will be in the final
-          iteration. Originally from Latin, Lorem ipsum has no intelligible
-          meaning. It is simply a display of letters to be viewed as a sample
-          with given graphical elements in a file. "Lipsum" (a portmanteau of
-          lorem and ipsum) generators are commonly used to form generic text in
-          a file. The words are adequately like normal text to demonstrate a
-          font, without distracting the reader with its content.
-        </p>
-      </div>
-      <div className="container-4">
-        <p>
-          Lorem ipsum is the filler text that typically demonstrates the font
-          and style of a text in a document or visual demonstration. It serves
-          as a place holder indicating where the text will be in the final
-          iteration. Originally from Latin, Lorem ipsum has no intelligible
-          meaning. It is simply a display of letters to be viewed as a sample
-          with given graphical elements in a file. "Lipsum" (a portmanteau of
-          lorem and ipsum) generators are commonly used to form generic text in
-          a file. The words are adequately like normal text to demonstrate a
-          font, without distracting the reader with its content.
-        </p>
-      </div>
-      <div className="container-5">
-        <p>
-          Lorem ipsum is the filler text that typically demonstrates the font
-          and style of a text in a document or visual demonstration. It serves
-          as a place holder indicating where the text will be in the final
-          iteration. Originally from Latin, Lorem ipsum has no intelligible
-          meaning. It is simply a display of letters to be viewed as a sample
-          with given graphical elements in a file. "Lipsum" (a portmanteau of
-          lorem and ipsum) generators are commonly used to form generic text in
-          a file. The words are adequately like normal text to demonstrate a
-          font, without distracting the reader with its content.
-        </p>
-      </div>
-      <div className="container-6">
-        <p>
-          Lorem ipsum is the filler text that typically demonstrates the font
-          and style of a text in a document or visual demonstration. It serves
-          as a place holder indicating where the text will be in the final
-          iteration. Originally from Latin, Lorem ipsum has no intelligible
-          meaning. It is simply a display of letters to be viewed as a sample
-          with given graphical elements in a file. "Lipsum" (a portmanteau of
-          lorem and ipsum) generators are commonly used to form generic text in
-          a file. The words are adequately like normal text to demonstrate a
-          font, without distracting the reader with its content.
-        </p>
+        {blogs.map((blog) =>(
+          <div key={blog.id}>
+            <p>title: {blog.title}</p>
+            <p>content: {blog.content}</p>
+            <p>blogger_id: {blog.blogger_id}</p>
+            </div>
+        ))}
       </div>
   
     </>
