d22d7eb0e4d9a803f88f91e2307aede24f26ce17
Bernardngari <bernardngari.ke@gmail.com>
2022-10-13 12:27:35 +0300

    Merge signup-login branch into bloggers

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,29 @@
-<<<<<<< HEAD
 
-import Navbar from './Component/Navbar';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-// import Home from './Component/Home';
-// import Blog from './Component/Blog';
+//import Navbar from './Component/Navbar';
+//// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import './App.css';
+//// import Home from './Component/Home';
+//// import Blog from './Component/Blog';
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      {/* <Router>
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="/blog" element={<Blog />} />
-    </Routes>
-    </Router> */}
-    </div>
-=======
-import './App.css';
-import Signup from './Signup';
-function App() {
-  return (
-    <div className="app">
-      <Signup/>
-   </div>
->>>>>>> d6bb063ccbc31439e573f0872dea26060efe8815
-  );
-}
+//function App() {
+//  return (
+//    <div>
+//      <Navbar />
+//      {/* <Router>
+//        <Routes>
+//          <Route exact path="/" element={<Home/>} />
+//          <Route exact path="/blog" element={<Blog />} />
+//    </Routes>
+//    </Router> */}
+//    </div>
+//import './App.css';
+//import Signup from './Signup';
+//function App() {
+//  return (
+//    <div className="app">
+//      <Signup/>
+//   </div>
+//  );
+//}
 
-export default App;
+//export default App;
