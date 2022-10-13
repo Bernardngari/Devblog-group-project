34a1243e7856d3ae1ff0c9a626243cea239eb6b0
Bernardngari <bernardngari.ke@gmail.com>
2022-10-13 12:14:05 +0300

    Merge blogs into bloggers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :bloggers
   post '/login', to: 'sessions#create_session'
   delete '/logout', to: 'sessions#logout'
-=======
   resources :blogs, only: [:index, :show, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
->>>>>>> 984d528ac0d5b231b4ed55781dc0ec5aade35e8e
 end
