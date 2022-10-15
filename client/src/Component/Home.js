b8ff3d33acd404eed11ac89ab9fafaef402c4bf1
Bernardngari <bernardngari.ke@gmail.com>
2022-10-16 00:39:55 +0300

    Fix bugs after merge

--- client/src/Component/Home.js
+++ client/src/Component/Home.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, NavLink } from "react-router-dom";
+import {  NavLink } from "react-router-dom";
 import capitalize from "./capitalize";
 function Home({onLogin,logout}) {
   const [blogs, setBlogs]= useState([]);
+  const [name, setName] = useState("")
   //const navigate = useNavigate();
   useEffect (() => {
     fetch("http://127.0.0.1:3000/blogs")
     .then((response) => response.json())
-    .then((blogs) => setBlogs(blogs));
+    .then((blogs) => {
+      setBlogs(blogs)
+    });
   }, [logout]);
 
   useEffect(()=>{
@@ -28,7 +31,6 @@ function Home({onLogin,logout}) {
             <p>{blog.content.substring(0,200)}...<NavLink to={`/blogs/${blog.id}`}>Read more</NavLink></p>
             <p><strong>Written by: {capitalize(blog.blogger.username)}</strong></p>
             </div>
-             
         ))}
       </div>
   
