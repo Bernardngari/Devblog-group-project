52047103e7e323ebdfbb01c7347765c1c86288ec
Bernardngari <bernardngari.ke@gmail.com>
2022-10-19 01:10:51 +0300

    change subclass on application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create_session
     blogger = Blogger.find_by(username: params[:username])
     if blogger&.authenticate(params[:password])
-      session[:blogger_id] = blogger.id 
+      session[:blogger_id] ||= blogger.id 
       render json:{ body: blogger, message: "Login success"}, status: :ok
     else
       render json: {message: "Wrong credentials. Please try again"}, status: :unauthorized
--- client/src/Component/Login.js
+++ client/src/Component/Login.js
@@ -13,7 +13,7 @@ const Login = ({onLogin}) => {
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value })
   }
-  const[error, setError] = useState("")
+  //const[error, setError] = useState("")
   function handleSubmit(e) {
     e.preventDefault()
     fetch("https://devbugger.herokuapp.com/login", {
@@ -28,16 +28,17 @@ const Login = ({onLogin}) => {
          navigate("/");
         r.json().then(data => onLogin(data.body.id))
       } else {
-        r.json().then((err) => setError(err.message));
+        r.json().then((err) => console.log(err.message));
       }
     });
     
   }
+  console.log(values);
   return (
       <div className="login">
       <form className="form" id="login" onSubmit={handleSubmit}>
       <p>Login</p>
-        <p className='error'>{ error}</p>
+        {/*<p className='error'>{ error}</p>*/}
               <div className="label"><label>Username</label></div>
               <input type="text" placeholder="username" name='username' onChange={handleChange} required/>
               <div className="label"><label htmlFor="password">Password</label></div>
@@ -46,18 +47,6 @@ const Login = ({onLogin}) => {
               <p className="message" >Not yet registered click <Link to={"/signup"}>here</Link> to signup</p>
           </form>
     </div>
-  //  <div className='container-2'>
-  //  <form id="login" onSubmit={handleSubmit}>
-  //    <p className='error'>{ error}</p>
-  //          <h3>Login Details</h3>
-  //          <label className='lab'>Username</label>
-  //          <input type="text" name='username' onChange={handleChange} required/>
-  //          <label className='lab' htmlFor="password">Password</label>
-  //          <input type="password" name='password' onChange={handleChange} required/>
-  //          <button className="btn2" type='submit'>Login</button>
-  //          <p>Not yet registered click <Link to={"/signup"}>here</Link> to signup</p>
-  //      </form>
-  //</div>
   )
 }
 export default Login
