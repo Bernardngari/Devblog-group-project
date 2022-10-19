58bf8003e79033a7e6b9e65d9dbcfc6960e23584
Bernardngari <bernardngari.ke@gmail.com>
2022-10-19 03:31:03 +0300

    .

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Devblog
 # Required for all session management (regardless of session_store)
   config.middleware.use ActionDispatch::Cookies
   config.middleware.use ActionDispatch::Session::CookieStore,
-  key: '_cookie_name', path: '/', same_site: :None, Secure
+  key: '_cookie_name', path: '/', same_site: :None, secure: :true
   config.middleware.use ActionDispatch::Flash
   config.middleware.use config.session_store, config.session_options
   config.middleware.use Rack::MethodOverride
